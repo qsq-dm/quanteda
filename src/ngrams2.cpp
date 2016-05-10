e239526a15ab774fa1f735a68b6b6821d70ea51c
koheiw <watanabe.kohei@gmail.com>
2016-05-10 13:09:56 +0100

    Fixed encoding for Windows

--- src/ngrams2.cpp
+++ src/ngrams2.cpp
@@ -46,6 +46,8 @@ void skip(const std::vector< string > &tokens,
     }
 }
 
+
+
 // [[Rcpp::export]]
 CharacterVector skipgram_cpp2(const vector < string > &tokens,
                               const vector < int > &ns, 
@@ -76,6 +78,82 @@ CharacterVector skipgram_cpp2(const vector < string > &tokens,
     }
     return tokens_ngram;
 }
+
+
+
+String join3(CharacterVector ngram, 
+             String delim){
+  if(ngram.size() == 0) return "";
+  String token_ngram = ngram[0];
+  int len_ngram = ngram.size();
+  for (int i = 1; i < len_ngram; i++) {
+    token_ngram += delim;
+    token_ngram += ngram[i];
+  }
+  return token_ngram;
+}
+
+void skip3(const CharacterVector &tokens,
+           const unsigned int start,
+           const unsigned int n, 
+           const std::vector< int > skips,
+           CharacterVector ngram,
+           std::vector<CharacterVector> &ngrams,
+           unsigned int e, unsigned int &f
+){
+  
+  //Rcout << "Start " << tokens[start] << " " << start << " " << n << "\n";
+  
+  ngram[e] = tokens[start];
+  e++;
+  
+  if(e < n){
+    for (int j = 0; j < skips.size(); j++){
+      int next = start + skips[j];
+      if(next > tokens.size() - 1) break;
+      skip3(tokens, next, n, skips, ngram, ngrams, e, f);
+    }
+  }else{
+    //Rcout << "Add " << join(ngram, "+")<< " " << n << " " << e << "\n";
+    ngrams[f] = ngram;
+    CharacterVector ngram(n);
+    e = 0;
+    f++;
+  }
+}
+
+// [[Rcpp::export]]
+CharacterVector skipgram_cpp3(SEXP x,
+                              const vector < int > &ns, 
+                              const vector < int > &skips, 
+                              const String &delim) {
+  
+  const CharacterVector tokens(x);
+  int len_ns = ns.size();
+  int len_skips = skips.size();
+  int len_tokens = tokens.size();
+  unsigned int e = 0; // Local index for tokens in ngram
+  unsigned int f = 0; // Global index for ngrams 
+  vector<CharacterVector> ngrams(len_ns * len_tokens * len_skips); // For the recursive function
+  
+  // Generate skipgrams recursively
+  for (int g = 0; g < len_ns; g++) {
+    int n = ns[g];
+    CharacterVector ngram(n);
+    for (int start = 0; start < len_tokens - (n - 1); start++) {
+      skip3(tokens, start, n, skips, ngram, ngrams, e, f); // Get ngrams as reference
+    }
+  }
+  
+  // Join elements of ngrams
+  CharacterVector tokens_ngram(f);
+  for (int k = 0; k < f; k++) {
+    tokens_ngram[k] = join3(ngrams[k], delim);
+  }
+  return tokens_ngram;
+}
+
+
 
 // [[Rcpp::export]]
 List skipgram_cppl2(SEXP x, 
